54c2ecb7c51e8bd27dc4812defe2fba8022967c2
Michaël Nass - PlugN <michael.nass@free.fr>
2022-05-02 18:17:58 +0200

    test

--- app2.js
+++ app2.js
@@ -1,16 +1,20 @@
-var app2 = new Vue({
-    el: '#app2',
-    data: {
-    todos: [],
-    },
-    mounted: function() {
-    let $vm = this;
-    fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(function (data) {
-        console.log('data', data)
-        data.forEach(function(item, index, array) {
-        console.log("UserID = " + item.userId + ", ID = " + item.id + ", Title = " + item.title + ", Completed = " + item.completed);
-        $vm.todos.push(item);
-        });
-    });
-    },
-});
+window.onload = function () {
+
+  var app2 = new Vue({
+      el: '#app2',
+      data: {
+      todos: [],
+      },
+      mounted: function() {
+      let $vm = this;
+      fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(function (data) {
+          console.log('data', data)
+          data.forEach(function(item, index, array) {
+          console.log("UserID = " + item.userId + ", ID = " + item.id + ", Title = " + item.title + ", Completed = " + item.completed);
+          $vm.todos.push(item);
+          });
+      });
+      },
+  });
+
+}
