e78d62fefcf112a02493c3995c92a6c2ac0da221
LaingMASTER7 <nassim.bellik32@gmail.com>
2022-05-02 16:31:38 +0200

    test2

---
+++ .history/app2_20220502163038.js
@@ -0,0 +1,16 @@
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+    todos: [],
+    },
+    mounted: function() {
+    let $vm = this;
+    fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(function (data) {
+        console.log('data', data)
+        data.forEach(function(item, index, array) {
+        console.log("UserID = " + item.userId + ", ID = " + item.id + ", Title = " + item.title + ", Completed = " + item.completed);
+        $vm.todos.push(item);
+        });
+    });
+    },
+});
