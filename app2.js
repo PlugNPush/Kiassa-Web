ec7f91ceeef6c8a84a1254c18367e7751e120eb0
Michaël Nass - PlugN <michael.nass@free.fr>
2022-05-22 20:49:04 +0200

    test NodeJS

--- app2.js
+++ app2.js
@@ -7,10 +7,12 @@ window.onload = function () {
       },
       mounted: function() {
       let $vm = this;
-      fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json()).then(function (data) {
+
+
+      fetch("https://api.kiassa.org/v1/search.php/", {method: "POST"}).then(response => response.json()).then(function (data) {
           console.log('data', data)
-          data.forEach(function(item, index, array) {
-          console.log("UserID = " + item.userId + ", ID = " + item.id + ", Title = " + item.title + ", Completed = " + item.completed);
+          data.data.forEach(function(item, index, array) {
+          console.log("Found item");
           $vm.todos.push(item);
           });
       });
